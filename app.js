69e3a1e212cdd0458960f3d6459489542ee6e566
pnguye11 <pnguye11@gmail.com>
2020-11-28 10:51:24 -0800

    DB added

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+require("dotenv/config");
 const app = express();
+
+///middlewares
 
 ///routes
 app.get("/", (req, res) => {
   res.send("we are live");
 });
+app.get("/posts", (req, res) => {
+  res.send("we are live");
+});
 
+////connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { userNewUrlParser: true }, () => {
+  console.log("connected to DB");
+});
 app.listen(3000);
